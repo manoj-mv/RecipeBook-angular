7ba51221aaf4da308103563c24a8c3f2c6b055e6
manoj-mv <manojmv3019@gmail.com>
2022-11-23 11:45:12 +0530

    recipes feature connverted to module

---
+++ src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AuthGaurd } from "../auth/auth.guard";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipeResolverService } from "./recipes-resolver.service";
+import { RecipesComponent } from "./recipes.component";
+
+
+const recipesRoutes: Routes = [
+    {
+        path: 'recipes',
+        component: RecipesComponent,
+        canActivate: [AuthGaurd],
+        children: [
+            { path: '', component: RecipeStartComponent },
+            { path: 'new', component: RecipeEditComponent },
+            { path: ':id', component: RecipeDetailComponent, resolve: [RecipeResolverService] },
+            { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipeResolverService] }
+        ]
+    },
+];
+@NgModule({
+    imports: [
+        RouterModule.forChild(recipesRoutes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class RecipesRoutingModule { }
